d28b30a130924f5a5fc7bd33d3feaf0295c6c3d9
Camice J. Revier <camice@users.noreply.github.com>
2015-07-21 16:37:19 +0100

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,5 +1,5 @@
 ## Script for R Programming / Week 2 / Assignment 1 - Air Polution
-## Pulutantmean Part 1
+## Part 1 - pullutantmean.R
 
 ## First we'll set the working directory more specifically:
 setwd(paste(getwd(), "Desktop/DataScienceTrack/R Programming", sep = "/"))
@@ -12,8 +12,8 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     files_list <- list.files(directory, full.names = TRUE) # To create a list of files.
     dat <- data.frame() # To create empty data frame, ready to be filled with our data.
     for (i in id) {
-        dat <- rbind(dat, read.csv(files_list[i]))
+        dat <- rbind(dat, read.csv(files_list[i])) # To create rbind loop for adding files.
     }
-    dat_sub <- dat[which(dat[, "ID"] %in% id),] # To create subset of data from `id` input.
-    mean(dat_sub[, pollutant], na.rm=TRUE)  # Identifies mean of pollutant while removing NAs.
+    dat_subset <- dat[which(dat[, "ID"] %in% id),] # To create subset of data from `id` input.
+    mean(dat_subset[, pollutant], na.rm=TRUE)  # Identifies mean of pollutant while removing NAs.
 }
